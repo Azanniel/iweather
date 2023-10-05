3fadfbedf64b9601c3011c2df3d01b19e6c34683
azanniel <leo.azannielttt@gmail.com>
2023-10-05 17:59:26 -0400

    ✨ add expo router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Page() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start, hello world!</Text>
+      <Text>Hello world from route based file!</Text>
       <StatusBar style="auto" />
     </View>
   );
