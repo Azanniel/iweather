76bdc3c9971461afb87ab8ec987b12c419cc8420
azanniel <leo.azannielttt@gmail.com>
2023-10-26 19:51:03 -0400

    ✅ test city storage

--- src/libs/storage/city-storage.spec.ts
+++ src/libs/storage/city-storage.spec.ts
@@ -1,8 +1,18 @@
-import { getStorageCity } from './city-storage'
+import { getStorageCity, saveStorageCity } from './city-storage'
 
 describe('Storage: City storage', () => {
   it('should returns null when city is not saved', async () => {
     const response = await getStorageCity()
     expect(response).toBeNull()
+  })
+
+  it('should be return city storage', async () => {
+    const city = { id: '1', name: 'Manaus', latitude: 123, longitude: 456 }
+
+    await saveStorageCity(city)
+    const response = await getStorageCity()
+
+    expect(response).toBeTruthy()
+    expect(response).toEqual(city)
   })
 })
