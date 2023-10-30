6fcd10fed009df321ff4766d6f1c538c7350a6d5
azanniel <leo.azannielttt@gmail.com>
2023-10-30 16:21:24 -0400

    ✅ test async storage to remove city

--- src/libs/storage/city-storage.spec.ts
+++ src/libs/storage/city-storage.spec.ts
@@ -1,4 +1,10 @@
-import { getStorageCity, saveStorageCity } from './city-storage'
+import {
+  getStorageCity,
+  removeStorageCity,
+  saveStorageCity,
+} from './city-storage'
+
+const city = { id: '1', name: 'Manaus', latitude: 123, longitude: 456 }
 
 describe('Storage: City storage', () => {
   it('should returns null when city is not saved', async () => {
@@ -7,12 +13,19 @@ describe('Storage: City storage', () => {
   })
 
   it('should be return city storage', async () => {
-    const city = { id: '1', name: 'Manaus', latitude: 123, longitude: 456 }
-
     await saveStorageCity(city)
     const response = await getStorageCity()
 
     expect(response).toBeTruthy()
     expect(response).toEqual(city)
+  })
+
+  it('should be remove city storage', async () => {
+    await saveStorageCity(city)
+    await removeStorageCity()
+
+    const response = await getStorageCity()
+
+    expect(response).toBeNull()
   })
 })
